ed5658bc61ce223553db0bb86e20dd82a9752ae9
LeviiLovie <leviilovie@gmail.com>
2022-08-01 22:04:03 +0700

    Vindigio is Vindigio_WR

--- src/Game.java
+++ src/Game.java
@@ -1,25 +1,20 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Game extends JFrame {
     JFrame frame = new JFrame();
+
+    Window window;
     public Game() {
         frame.setTitle("Vindigio");
         frame.setLocation(0, 0);
         frame.setSize(1280, 720 + 28);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.add(new Window());
+        frame.add(window = new Window());
         frame.setVisible(true);
 
         Timer timer;
-        timer = new Timer(0, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frame.repaint();
-            }
-        });
+        timer = new Timer(0, e -> frame.repaint());
         timer.setRepeats(true);
         // Aprox. 60 FPS
         timer.setDelay(17);
--- src/Window.java
+++ src/Window.java
@@ -1,12 +1,13 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 
 public class Window extends JPanel implements KeyListener {
     World world;
@@ -18,22 +19,46 @@ public class Window extends JPanel implements KeyListener {
     BufferedImage img5 = null;
     BufferedImage img6 = null;
 
+    private JButton world_constructor_button = null;
+    private JButton world_redactor_button;
+    private final JButton exit_button;
+    private final JButton save_button;
+
     public Window() {
         world = new World();
 
-        try {img0 = ImageIO.read(new File("src/Tiles/0.png"));}  catch (IOException e) {}
-        try {img1 = ImageIO.read(new File("src/Tiles/1.jpeg"));} catch (IOException e) {}
-        try {img2 = ImageIO.read(new File("src/Tiles/2.jpeg"));} catch (IOException e) {}
-        try {img3 = ImageIO.read(new File("src/Tiles/3.jpeg"));} catch (IOException e) {}
-        try {img4 = ImageIO.read(new File("src/Tiles/4.jpeg"));} catch (IOException e) {}
-        try {img5 = ImageIO.read(new File("src/Tiles/5.jpeg"));} catch (IOException e) {}
-        try {img6 = ImageIO.read(new File("src/Tiles/6.jpeg"));} catch (IOException e) {}
+        try {img0 = ImageIO.read(new File("src/Tiles/0.png"));}  catch (IOException ignored) {}
+        try {img1 = ImageIO.read(new File("src/Tiles/1.jpeg"));} catch (IOException ignored) {}
+        try {img2 = ImageIO.read(new File("src/Tiles/2.jpeg"));} catch (IOException ignored) {}
+        try {img3 = ImageIO.read(new File("src/Tiles/3.jpeg"));} catch (IOException ignored) {}
+        try {img4 = ImageIO.read(new File("src/Tiles/4.jpeg"));} catch (IOException ignored) {}
+        try {img5 = ImageIO.read(new File("src/Tiles/5.jpeg"));} catch (IOException ignored) {}
+        try {img6 = ImageIO.read(new File("src/Tiles/6.jpeg"));} catch (IOException ignored) {}
 
         JPanel panel = new JPanel();
 //        world.world_constructor();
         panel.setFocusable(true);
         panel.requestFocusInWindow();
         panel.addKeyListener(this);
+        panel.setVisible(true);
+
+        this.world_redactor_button = new JButton("World redactor mode");
+        this.world_redactor_button.setVisible(false);
+
+        this.world_constructor_button = new JButton("Auto world generate");
+        this.world_constructor_button.setVisible(false);
+
+        this.exit_button = new JButton("Exit");
+        this.exit_button.setVisible(false);
+
+        this.save_button = new JButton("Save");
+        this.save_button.setVisible(false);
+
+        add(world_redactor_button); // TODO comment this line for finis compilation
+        add(this.world_constructor_button);
+        add(this.exit_button);
+        add(this.save_button);
+
         add(world);
         add(panel);
     }
@@ -51,13 +76,17 @@ public class Window extends JPanel implements KeyListener {
                 x = i + this.world.screen_x;
                 y = j + this.world.screen_y;
                 if (this.world.json.world_tiles[y][x] == 1) {
-                    g2d.drawImage(this.img1, i * sprite_size, j * sprite_size, null);
+                    g2d.drawImage(this.img1, (i * sprite_size), (j * sprite_size), null);
                 } else if (this.world.json.world_tiles[y][x] == 2) {
-                    g2d.drawImage(this.img2, i * sprite_size, j * sprite_size, null);
+                    g2d.drawImage(this.img2, (i * sprite_size), (j * sprite_size), null);
                 } else if (this.world.json.world_tiles[y][x] == 3) {
-                    g2d.drawImage(this.img3, i * sprite_size, j * sprite_size, null);
-                } else {
-                    g2d.drawImage(this.img4, i * sprite_size, j * sprite_size, null);
+                    g2d.drawImage(this.img3, (i * sprite_size), (j * sprite_size), null);
+                } else if (this.world.json.world_tiles[y][x] == 4) {
+                    g2d.drawImage(this.img4, (i * sprite_size), (j * sprite_size), null);
+                } else if (this.world.json.world_tiles[y][x] == 5) {
+                    g2d.drawImage(this.img5, (i * sprite_size), (j * sprite_size), null);
+                } else if (this.world.json.world_tiles[y][x] == 6) {
+                    g2d.drawImage(this.img6, (i * sprite_size), (j * sprite_size), null);
                 }
             }
         }
@@ -66,9 +95,48 @@ public class Window extends JPanel implements KeyListener {
         int player_screen_y = this.world.get_player_screen_y();
         g2d.drawImage(this.img0, player_screen_x * sprite_size, player_screen_y * sprite_size,null);
 
+        g2d.drawImage(this.img1, 1250, 0, null);
+        g2d.drawImage(this.img2, 1250, 32, null);
+        g2d.drawImage(this.img3, 1250, 64, null);
+        g2d.drawImage(this.img4, 1250, 96, null);
+        g2d.drawImage(this.img5, 1250, 128, null);
+        g2d.drawImage(this.img6, 1250, 160, null);
+
         if (this.world.pause) {
             g2d.setColor(new Color(255, 255, 255, 155));
-            g2d.fillRect(0, 0, this.world.screen_width, this.world.screen_height);
+            g2d.fillRect(0, 0, 1280, 720);
+
+            if (this.world.json.mode == 1) { // TODO
+                this.world_constructor_button.setVisible(true);
+            }
+            if (this.world.json.mode == 0) {
+                this.world_redactor_button.setVisible(true);
+            }
+            this.exit_button.setVisible(true);
+            this.save_button.setVisible(true);
+
+            if (this.world.json.mode == 1) { // TODO
+                this.world_constructor_button.addActionListener(e -> world.world_constructor());
+            }
+            this.exit_button.addActionListener(e -> world.pause = false);
+            this.save_button.addActionListener(e -> {
+//                    world.json.set_world();
+                System.out.println("Save in developing(");
+            });
+            this.world_redactor_button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    world.json.mode = 1;
+                    System.out.println(world.json.mode);
+                }
+            });
+        } else {
+            this.world_redactor_button.setVisible(false);
+            if (this.world_constructor_button != null) {
+                this.world_constructor_button.setVisible(false);
+            }
+            this.exit_button.setVisible(false);
+            this.save_button.setVisible(false);
         }
     }
 
